f8f2cb4c89ad4c2fc7d4cf1df6e66b4fd1c72459
Manuel Esteban (Merb) <manuele.ramirez.26@gmail.com>
2023-12-18 10:59:22 -0600

    modified App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="container-app">
+        <div className="container-app__title">Cremería</div>
+        <button>Registrar piezas faltantes</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
