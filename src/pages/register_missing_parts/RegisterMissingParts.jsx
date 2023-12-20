934366c396e188794c00938043262bc3e1a322ce
Manuel Esteban (Merb) <manuele.ramirez.26@gmail.com>
2023-12-20 12:08:39 -0600

    add useState on form

--- src/pages/register_missing_parts/RegisterMissingParts.jsx
+++ src/pages/register_missing_parts/RegisterMissingParts.jsx
@@ -1,9 +1,22 @@
+import { useState } from 'react';
 import './styles.css';
 
 export const RegisterMissingParts = () => {
+  const [name, setName] = useState('');
+  const [amountPieces, setAmountPieces] = useState('')
+  const [totalCost, setTotalCost] = useState('')
+  const [unitCost, setUnitCost] = useState('')
+  const [publicPrice, setPublicPrice] = useState('')
+  const [revenue, setRevenue] = useState('')
 
-  const handleRegister = () => {
-    alert('registrando.......');
+  const handleRegister = (event) => {
+    event.preventDefault();
+    console.log({ name });
+    console.log({ amountPieces });
+    console.log({ totalCost });
+    console.log({ unitCost });
+    console.log({ publicPrice });
+    console.log({ revenue });
   };
 
   return (
@@ -11,44 +24,54 @@ export const RegisterMissingParts = () => {
       <div className="container-register-missing__title">
         Registrando piezas faltantes
       </div>
-      <form onSubmit={handleRegister} className="container-register-missing__box">
+      <form
+        onSubmit={handleRegister}
+        className="container-register-missing__box"
+      >
         <div>
           <label htmlFor="name" className="input-field">
             Nombre:{' '}
           </label>
-          <input type="text" id="name" name="name" required />
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
         </div>
         <div>
           <label htmlFor="amountPieces" className="input-field">
             Cantidad de piezas:{' '}
           </label>
-          <input type="text" id="amountPieces" name="amountPieces" required />
+          <input type="text" id="amountPieces" name="amountPieces" value={amountPieces} onChange={(e) => setAmountPieces(e.target.value)} required />
         </div>
         <div>
           <label htmlFor="totalCost" className="input-field">
             Costo total:{' '}
           </label>
-          <input type="text" id="totalCost" name="totalCost" required />
+          <input type="text" id="totalCost" name="totalCost" value={totalCost} onChange={(e) => setTotalCost(e.target.value)} required />
         </div>
         <div>
           <label htmlFor="unitCost" className="input-field">
             Costo unitario:{' '}
           </label>
-          <input type="text" id="unitCost" name="unitCost" required />
+          <input type="text" id="unitCost" name="unitCost" value={unitCost} onChange={(e) => setUnitCost(e.target.value)} required />
         </div>
         <div>
           <label htmlFor="publicPrice" className="input-field">
             Precio al público:{' '}
           </label>
-          <input type="text" id="publicPrice" name="publicPrice" required />
+          <input type="text" id="publicPrice" name="publicPrice" value={publicPrice} onChange={(e) => setPublicPrice(e.target.value)} required />
         </div>
         <div>
           <label htmlFor="revenue" className="input-field">
             Ganancia:{' '}
           </label>
-          <input type="text" id="revenue" name="revenue" required />
+          <input type="text" id="revenue" name="revenue" value={revenue} onChange={(e) => setRevenue(e.target.value)} required />
         </div>
-        <button type='submit'>Registrar</button>
+        <button type="submit">Registrar</button>
       </form>
     </div>
   );
