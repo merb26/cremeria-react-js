7d7f38c547ddd7333df8d6e218a803e03fe8c47e
Manuel Esteban (Merb) <manuele.ramirez.26@gmail.com>
2023-12-30 22:54:04 -0600

    working when saving record

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 import { Link } from 'react-router-dom';
+import { initializeApp } from 'firebase/app';
 import './App.css';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDXJnCSLQD2o5lqmaPW1AF5F59kPDJcvWk',
+  authDomain: 'cremeria-react-js.firebaseapp.com',
+  projectId: 'cremeria-react-js',
+  storageBucket: 'cremeria-react-js.appspot.com',
+  messagingSenderId: '546941054738',
+  appId: '1:546941054738:web:949b4242e8a7c249f30dcb',
+};
+
+initializeApp(firebaseConfig);
 
 function App() {
   return (
--- src/pages/register_missing_parts/RegisterMissingParts.jsx
+++ src/pages/register_missing_parts/RegisterMissingParts.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
+import { addDoc, collection, getFirestore } from 'firebase/firestore';
 import './styles.css';
 
 export const RegisterMissingParts = () => {
   const [name, setName] = useState('');
-  const [amountPieces, setAmountPieces] = useState('')
-  const [totalCost, setTotalCost] = useState('')
-  const [unitCost, setUnitCost] = useState('')
-  const [publicPrice, setPublicPrice] = useState('')
-  const [revenue, setRevenue] = useState('')
+  const [amountPieces, setAmountPieces] = useState('');
+  const [totalCost, setTotalCost] = useState('');
+  const [unitCost, setUnitCost] = useState('');
+  const [publicPrice, setPublicPrice] = useState('');
+  const [revenue, setRevenue] = useState('');
+  const [id, setId] = useState('');
 
   const handleRegister = (event) => {
     event.preventDefault();
@@ -17,6 +19,27 @@ export const RegisterMissingParts = () => {
     console.log({ unitCost });
     console.log({ publicPrice });
     console.log({ revenue });
+
+    const register = {
+      name,
+      amountPieces,
+      totalCost,
+      unitCost,
+      publicPrice,
+      revenue,
+    };
+
+    const db = getFirestore();
+    const registerCollection = collection(db, 'registers');
+    const query = addDoc(registerCollection, register);
+
+    query
+      .then(({ id }) => {
+        setId(id);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return (
@@ -45,31 +68,66 @@ export const RegisterMissingParts = () => {
           <label htmlFor="amountPieces" className="input-field">
             Cantidad de piezas:{' '}
           </label>
-          <input type="text" id="amountPieces" name="amountPieces" value={amountPieces} onChange={(e) => setAmountPieces(e.target.value)} required />
+          <input
+            type="text"
+            id="amountPieces"
+            name="amountPieces"
+            value={amountPieces}
+            onChange={(e) => setAmountPieces(e.target.value)}
+            required
+          />
         </div>
         <div>
           <label htmlFor="totalCost" className="input-field">
             Costo total:{' '}
           </label>
-          <input type="text" id="totalCost" name="totalCost" value={totalCost} onChange={(e) => setTotalCost(e.target.value)} required />
+          <input
+            type="text"
+            id="totalCost"
+            name="totalCost"
+            value={totalCost}
+            onChange={(e) => setTotalCost(e.target.value)}
+            required
+          />
         </div>
         <div>
           <label htmlFor="unitCost" className="input-field">
             Costo unitario:{' '}
           </label>
-          <input type="text" id="unitCost" name="unitCost" value={unitCost} onChange={(e) => setUnitCost(e.target.value)} required />
+          <input
+            type="text"
+            id="unitCost"
+            name="unitCost"
+            value={unitCost}
+            onChange={(e) => setUnitCost(e.target.value)}
+            required
+          />
         </div>
         <div>
           <label htmlFor="publicPrice" className="input-field">
             Precio al público:{' '}
           </label>
-          <input type="text" id="publicPrice" name="publicPrice" value={publicPrice} onChange={(e) => setPublicPrice(e.target.value)} required />
+          <input
+            type="text"
+            id="publicPrice"
+            name="publicPrice"
+            value={publicPrice}
+            onChange={(e) => setPublicPrice(e.target.value)}
+            required
+          />
         </div>
         <div>
           <label htmlFor="revenue" className="input-field">
             Ganancia:{' '}
           </label>
-          <input type="text" id="revenue" name="revenue" value={revenue} onChange={(e) => setRevenue(e.target.value)} required />
+          <input
+            type="text"
+            id="revenue"
+            name="revenue"
+            value={revenue}
+            onChange={(e) => setRevenue(e.target.value)}
+            required
+          />
         </div>
         <button type="submit">Registrar</button>
       </form>
